78e86f24e217e08d70146b85bc095541f02efb11
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-03-01 10:47:27 +0100

    tesxting qt

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -1,14 +1,24 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Connect button click event to slot function
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(onButtonClicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+// Slot function for button click
+void MainWindow::onButtonClicked()
+{
+    // Update label text when the button is clicked
+    ui->label->setText("Button Clicked!");
 }
